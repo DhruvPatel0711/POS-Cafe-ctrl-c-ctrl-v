cbe4249382a16e51c57eb5dba63b9cce4590423f
Dhruv Patel <Dhruv11072005@gmail.com>
2026-04-04 14:36:07 +0530

    Task 0

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 
-const prisma = new PrismaClient();
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 const SALT_ROUNDS = 10;
 
